d97825d28aabaddbbf153dbaf3c45cfbbe8d2b89
mdekauwe <mdekauwe@gmail.com>
2016-04-30 00:37:58 +1000

    cleanup

--- plot_data.R
+++ plot_data.R
@@ -3,19 +3,18 @@
 library("ggplot2")
 require(mgcv)
 
-url <- "http://www.metoffice.gov.uk/hadobs/hadcrut4/data/current/" + \
+url <- "http://www.metoffice.gov.uk/hadobs/hadcrut4/data/current/" +\
        "time_series/HadCRUT.4.4.0.0.annual_ns_avg.txt"
-had_cru <- read.table(url, fill=TRUE)
+cru <- read.table(url, fill=TRUE)
 
 # drop the other cols
-had_cru <- had_cru[c(1,2)]
-names(had_cru)[1] <- "year"
-names(had_cru)[2] <- "temp"
+cru <- cru[c(1,2)]
+names(cru)[1] <- "year"
+names(cru)[2] <- "temp"
 
-g <-gam(year~s(temp), data=had_cru)
-#gam.check(g)
+g <-gam(year~s(temp), data=cru)
 
-ggplot(had_cru, aes(year, temp)) +
+ggplot(cru, aes(year, temp)) +
        geom_point(size=0.8) +
        xlab("Year") +
        ylab("Temperature anomaly (1961-1990) deg C") +
