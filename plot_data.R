c6d0535966c43d63e85894264544dbf67af640c7
mdekauwe <mdekauwe@gmail.com>
2016-04-30 09:56:58 +1000

    Cleaned up plot

--- plot_data.R
+++ plot_data.R
@@ -3,7 +3,9 @@
 library("ggplot2")
 require(mgcv)
 
-url <- "http://www.metoffice.gov.uk/hadobs/hadcrut4/data/current/" +\
+golden_ratio <- 1.0 / 1.6180339887
+
+url <- "http://www.metoffice.gov.uk/hadobs/hadcrut4/data/current/" + "
        "time_series/HadCRUT.4.4.0.0.annual_ns_avg.txt"
 cru <- read.table(url, fill=TRUE)
 
@@ -12,10 +14,8 @@ cru <- cru[c(1,2)]
 names(cru)[1] <- "year"
 names(cru)[2] <- "temp"
 
-g <-gam(year~s(temp), data=cru)
-
 ggplot(cru, aes(year, temp)) +
        geom_point(size=0.8) +
        xlab("Year") +
-       ylab("Temperature anomaly (1961-1990) deg C") +
-       stat_smooth(method="gam", formula=y~s(x), size=1)
+       ylab("Temperature anomaly (1961-1990; deg C)") +
+       theme(aspect.ratio=golden_ratio)
