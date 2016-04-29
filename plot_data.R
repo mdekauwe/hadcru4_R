546917b5d946c0d7f0ccbb364be2caf14de0b43d
mdekauwe <mdekauwe@gmail.com>
2016-04-29 23:59:31 +1000

    Removed rowname label

--- plot_data.R
+++ plot_data.R
@@ -1,6 +1,7 @@
 #!/usr/bin/Rscript
 
 library("ggplot2")
+require(mgcv)
 
 url <- "http://www.metoffice.gov.uk/hadobs/hadcrut4/data/current/" + \
        "time_series/HadCRUT.4.4.0.0.annual_ns_avg.txt"
@@ -11,11 +12,11 @@ had_cru <- had_cru[c(1,2)]
 names(had_cru)[1] <- "year"
 names(had_cru)[2] <- "temp"
 
-require(mgcv)
+g <-gam(year~s(temp), data=had_cru)
+#gam.check(g)
 
-p <- ggplot(had_cru, aes(year, temp)) +
-            geom_point(size=0.8) +
-            xlab("Year") +
-            ylab("Temperature anomaly (1961-1990) deg C")
-
-p + stat_smooth(method="gam", formula=y~s(x, k=-1), size=1)
+ggplot(had_cru, aes(year, temp)) +
+       geom_point(size=0.8) +
+       xlab("Year") +
+       ylab("Temperature anomaly (1961-1990) deg C") +
+       stat_smooth(method="gam", formula=y~s(x), size=1)
