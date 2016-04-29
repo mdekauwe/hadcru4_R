223dfcc12ca39bbe54740e9a2556f33fc80fc7a1
mdekauwe <mdekauwe@gmail.com>
2016-04-29 23:46:09 +1000

    Removed rowname label

--- plot_data.R
+++ plot_data.R
@@ -14,8 +14,8 @@ names(had_cru)[2] <- "temp"
 require(mgcv)
 
 p <- ggplot(had_cru, aes(year, temp)) +
-            geom_point() +
+            geom_point(size=0.8) +
             xlab("Year") +
             ylab("Temperature anomaly (1961-1990) deg C")
 
-p + stat_smooth(method="gam", formula=y~s(x, k=3), size=1)
+p + stat_smooth(method="gam", formula=y~s(x, k=-1), size=1)
