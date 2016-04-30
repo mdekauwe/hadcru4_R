af2d177e506a07023a76391000f0f66a4889d378
mdekauwe <mdekauwe@gmail.com>
2016-04-30 13:43:14 +1000

    Few tweaks to plot layout

--- plot_data.R
+++ plot_data.R
@@ -1,9 +1,6 @@
 #!/usr/bin/Rscript
-
 library("ggplot2")
 require(mgcv)
-
-golden_ratio <- 1.0 / 1.6180339887
 
 fn <- "HadCRUT_data.txt"
 cru <- read.table(url, fill=TRUE)
@@ -13,10 +10,13 @@ cru <- cru[c(1,2)]
 names(cru)[1] <- "year"
 names(cru)[2] <- "temp"
 
+golden_ratio <- 1.0 / 1.6180339887
 ggplot(cru, aes(year, temp)) +
        geom_point(size=0.8) +
        xlab("Year") +
-       ylab("Temperature anomaly (1961-1990; deg C)") +
-       stat_smooth(method="gam", formula=y~s(x), size=1) +
+       ylab(expression(Temperature~anomaly~(~degree~C)~1961-1990)) +
+       stat_smooth(method="gam", formula=y~s(x, k=20), size=0.7) +
        theme_bw() +
-       theme(aspect.ratio=golden_ratio)
+       theme(aspect.ratio=golden_ratio,
+             panel.grid.major=element_blank(),
+             panel.grid.minor=element_blank())
