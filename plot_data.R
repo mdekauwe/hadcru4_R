47b59c4c6530bbfaf719255287634ab6a052464c
mdekauwe <mdekauwe@gmail.com>
2016-04-30 17:06:54 +1000

    Added ggplot gam

--- plot_data.R
+++ plot_data.R
@@ -1,6 +1,7 @@
 #!/usr/bin/Rscript
 library("ggplot2")
 require(mgcv)
+require(gridExtra)
 
 fn <- "HadCRUT_data.txt"
 cru <- read.table(url, fill=TRUE)
@@ -11,16 +12,16 @@ names(cru)[1] <- "year"
 names(cru)[2] <- "temp"
 
 golden_ratio <- 1.0 / 1.6180339887
-#ggplot(cru, aes(year, temp)) +
-#       geom_point(size=0.8) +
-#       xlab("Year") +
-#       ylab(expression(Temperature~anomaly~(~degree~C)~1961-1990)) +
-#       geom_hline(yintercept=0.0, linetype="dashed", colour="lightgrey") +
-#       stat_smooth(method="gam", formula=y~s(x, k=20), size=0.7) +
-#       theme_bw() +
-#       theme(aspect.ratio=golden_ratio,
-#             panel.grid.major=element_blank(),
-#             panel.grid.minor=element_blank())
+ax1 <- ggplot(cru, aes(year, temp)) +
+         geom_point(size=0.8) +
+         xlab("") +
+         ylab(expression(Temperature~anomaly~(~degree~C)~1961-1990)) +
+         geom_hline(yintercept=0.0, linetype="dashed", colour="lightgrey") +
+         stat_smooth(method="gam", formula=y~s(x, k=20), size=0.7) +
+         theme_bw() +
+         theme(aspect.ratio=golden_ratio,
+               panel.grid.major=element_blank(),
+               panel.grid.minor=element_blank())
 
 # Check if AR1 model is a better fit
 g1 <- gamm(temp~s(year, k=20), data=cru)
@@ -37,14 +38,17 @@ df <- with(cru, data.frame(year=seq(min(year), max(year), length=length(year)),
                            lcl=ar1 - 1.96 * se,
                            ucl=ar1 + 1.96 * se))
 
-ggplot() +
-  geom_point(data=cru, aes(year, temp), size=0.8) +
-  geom_line(data=df, aes(year, temp), size=0.8, colour="blue") +
-  geom_ribbon(data=df, aes(year, ymin=lcl, ymax=ucl), fill="grey", alpha=.4) +
-  ylab(expression(Temperature~anomaly~(~degree~C)~1961-1990)) +
-  geom_hline(yintercept=0.0, linetype="dashed", colour="lightgrey") +
-  stat_smooth(method="gam", formula=y~s(x, k=20), size=0.7) +
-  theme_bw() +
-  theme(aspect.ratio=golden_ratio,
-       panel.grid.major=element_blank(),
-       panel.grid.minor=element_blank())
+ax2 <- ggplot() +
+         geom_point(data=cru, aes(year, temp), size=0.8) +
+         geom_line(data=df, aes(year, temp), size=0.8, colour="blue") +
+         geom_ribbon(data=df, aes(year, ymin=lcl, ymax=ucl),
+                     fill="grey", alpha=.4) +
+         ylab(expression(Temperature~anomaly~(~degree~C)~1961-1990)) +
+         geom_hline(yintercept=0.0, linetype="dashed", colour="lightgrey") +
+         stat_smooth(method="gam", formula=y~s(x, k=20), size=0.7) +
+         theme_bw() +
+         theme(aspect.ratio=golden_ratio,
+              panel.grid.major=element_blank(),
+              panel.grid.minor=element_blank())
+
+grid.arrange(ax1, ax2, ncol=1)
