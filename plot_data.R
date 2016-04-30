15d299a8e27ae0e7874ad8c7c17647e1d719dda8
mdekauwe <mdekauwe@gmail.com>
2016-04-30 17:08:41 +1000

    Added gam to second plot

--- plot_data.R
+++ plot_data.R
@@ -30,17 +30,21 @@ g2 <- gamm(temp~s(year, k=20), data=cru, correlation=corARMA(form=~year, p=1))
 # AR1 model is best.
 anova(g1$lme, g2$lme)
 
-#gam <- predict(g1$gam, data=cru)$fit
+gam <- predict(g1$gam, data=cru)$fit
 ar1 <- predict(g2$gam, data=cru)
 se <- predict(g2$gam, data=cru, se=TRUE)$se.fit
 df <- with(cru, data.frame(year=seq(min(year), max(year), length=length(year)),
                            temp=ar1,
                            lcl=ar1 - 1.96 * se,
                            ucl=ar1 + 1.96 * se))
+df2 <- with(cru, data.frame(year=seq(min(year), max(year), length=length(year)),
+                           temp=gam))
+
 
 ax2 <- ggplot() +
          geom_point(data=cru, aes(year, temp), size=0.8) +
          geom_line(data=df, aes(year, temp), size=0.8, colour="blue") +
+         geom_line(data=df2, aes(year, temp), size=0.8, colour="lightgreen") +
          geom_ribbon(data=df, aes(year, ymin=lcl, ymax=ucl),
                      fill="grey", alpha=.4) +
          ylab(expression(Temperature~anomaly~(~degree~C)~1961-1990)) +
